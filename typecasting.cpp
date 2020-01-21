0d92a55c07fc9c91d432cfe11733016be2ae8f11
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-21 22:59:40 +0530

    Typecasting of double to float

---
+++ typecasting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=32;
+    float f=34.56f;//By default it is double
+    double d=34.56;
+    cout<<"Int is "<<a<<endl;
+    cout<<"Float is "<<f<<endl;
+    cout<<"Double is "<<d<<endl;
+}
