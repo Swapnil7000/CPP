2f99f6642c702243892a0a31baa2667c588d4c00
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-21 22:49:47 +0530

    first commit

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a, b;
+   cout << "Enter the two numbers:";
+   cin >> a;
+   cin >> b;
+   cout << "Sum is " << a + b;
+   return 0;
+}
---
+++ var.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float f = 4.57;
+    int a = 5;
+    bool b = false;
+    double d = 34.2564;
+    cout << "Float is " << f << "\n";
+    cout << "Integer is  " << a << "\n";
+    cout << "Boolean is  " << b << "\n";
+    cout << "Double is " << d << "\n";
+    return 0;
+}
