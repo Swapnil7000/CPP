3a49244e77a2d780377fc761a1ef7a48af5631ae
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-23 22:58:51 +0530

    While loop added

---
+++ loop1.cpp
@@ -0,0 +1,15 @@
+// Printing numbers from 1 till n
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
