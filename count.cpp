6cbf5fcbfab1901cffa6e0d72ff6dc8527d44b8a
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-26 11:59:35 +0530

    Program to count vowels,consonants,digits,spaces in a string

---
+++ count.cpp
@@ -0,0 +1,35 @@
+//Program to count vowels,digits,consonants,whitespaces
+#include<iostream>
+using namespace std;
+int main()
+{
+string str;
+int i;
+int vowelCount=0,consonantCount=0,digitCount=0,spacesCount=0;
+cout<<"Enter the string:";
+getline(cin,str);
+for(i=0;i<str.length();i++)
+{
+  if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+  {
+    ++vowelCount;
+  }
+  if(str[i]>='0'&&str[i]<='9')
+  {
+      ++digitCount;
+  }
+  if(str[i]>'A'&&str[i]<='Z'||str[i]>'a'||str[i]<'z')
+  {
+      ++consonantCount;
+  }
+  if(str[i]==' ')
+  {
+      ++spacesCount;
+  }
+}
+cout<<"Number of vowels in string are:"<<vowelCount<<endl;
+cout<<"Number of consonants in string are:"<<consonantCount<<endl;
+cout<<"Number of digits in string are:"<<digitCount<<endl;
+cout<<"Number of spaces in string are:"<<spacesCount<<endl;
+return 0;
+}
