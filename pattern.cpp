bd859c5385462e4ca045d59b17348f2b6c6a93fb
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-27 18:21:36 +0530

    To print the triangle pattern as commented in the code

---
+++ pattern.cpp
@@ -0,0 +1,24 @@
+//To print the pattern like this
+/*
+  1
+  22
+  333
+  4444
+  55555 */
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j,n;
+cout<<"Enter the number of rows:"<<endl;
+cin>>n;
+for(i=1;i<=n;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        cout<<i;
+    }
+    cout<<endl;
+}
+return 0;
+}
