4a1831f50ffe54be0db1cc40fb8e72ddd87c5ad7
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-28 21:59:36 +0530

    ternary operators in c++

---
+++ ternaryOperator.cpp
@@ -0,0 +1,12 @@
+// To check condition using ternary operators
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"Enter the two numbers a and b:";
+ cin>>a;
+ cin>>b;
+ a>b?cout<<"a is greater than b":cout<<"a is lesser than b";
+ return 0;
+}
