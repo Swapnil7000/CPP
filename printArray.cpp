df27a8c8e293f8e2f99540dbf7c23b8c959556bb
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-29 21:44:26 +0530

    To print array elements

--- printArray.cpp
+++ printArray.cpp
@@ -1,10 +1,12 @@
-// To change array elements 
+// To print array elements using loop
 #include<iostream>
 using namespace std;
 int main()
 {
 string name[]={"Swapnil","Sakeena","Shubham"};
-name[1]="Swapnil";
-cout<<name[1];
+for(int i=0;i<3;i++)
+{
+    cout<<name[i]<<endl;
+}
 return 0;
 }
