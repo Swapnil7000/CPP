0eb2a25823766646de5f7a176c6aa95285c27155
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-29 21:35:59 +0530

    To change an array element

---
+++ changeArray.cpp
@@ -0,0 +1,12 @@
+// To change array elements 
+#include<iostream>
+using namespace std;
+int main()
+{
+string name[]={"Swapnil","Sakeena","Shubham"};
+name[1]="Swapnil";
+cout<<name[1];
+
+
+return 0;
+}
