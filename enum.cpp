28220106b4a8e323ce31ba0132bc21ccd25ba8f8
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-30 22:26:34 +0530

    To print integer assigned to enum data

---
+++ enum.cpp
@@ -0,0 +1,9 @@
+// To print data using enum
+#include<iostream>
+using namespace std;
+int main()
+{
+enum weekDays {sunday,monday,tuesday,wednesday,thrusday,friday,saturday};
+cout<<sunday;
+return 0;
+}
