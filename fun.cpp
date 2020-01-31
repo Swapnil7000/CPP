6ad14b1aa9c5177a195ae9017d37ed8c9d0ef4b9
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-31 21:41:22 +0530

    Now,printing the whole name

--- fun.cpp
+++ fun.cpp
@@ -1,15 +1,15 @@
 // To print a message using a function
 #include <iostream>
+using namespace std;
 void g(void)
 {
-    std::cout << "Good morning!!";
+    cout << "Good morning!!";
 }
-using namespace std;
 int main()
 {
     string myName;
     cout << "Enter your name" << endl;
-    cin >> myName;
+    getline(cin,myName);
     g();
     cout << myName;
     return 0;
