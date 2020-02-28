52aaef89ec336c34ad3b246aad60065760cfb1c9
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-28 22:21:57 +0530

    Formatted the code

--- checkOccurences.cpp
+++ checkOccurences.cpp
@@ -1,26 +1,26 @@
 // To check occurences of a given number in an array
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-int n,key_num;
-cout<<"Enter the array size "<<endl;
-cin>>n;
-int arr[n],i,count=0;
-cout<<"Enter the array elements"<<endl;
-for(i=0;i<n;i++)
-{
-    cin>>arr[i];
-}
-cout<<"Enter the number whose occurences you have to find"<<endl;
-cin>>key_num;
-for(i=0;i<n;i++)
-{
-    if(arr[i]==key_num)
+    int n, key_num;
+    cout << "Enter the array size " << endl;
+    cin >> n;
+    int arr[n], i, count = 0;
+    cout << "Enter the array elements" << endl;
+    for (i = 0; i < n; i++)
     {
-        count++;
+        cin >> arr[i];
     }
-}
-cout<<key_num<<" occurs "<<count<<" times in the array";
-return 0;
+    cout << "Enter the number whose occurences you have to find" << endl;
+    cin >> key_num;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == key_num)
+        {
+            count++;
+        }
+    }
+    cout << key_num << " occurs " << count << " times in the array";
+    return 0;
 }
