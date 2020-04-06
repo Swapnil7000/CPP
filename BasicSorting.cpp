316e6f786db2d276c4348e9c9c58409701e8eb05
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-06 16:12:12 +0530

    Basic sorting using stl

---
+++ BasicSorting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, j;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+                swap(a[j], a[i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
